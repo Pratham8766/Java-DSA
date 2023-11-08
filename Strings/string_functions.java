925ae0e90ff0c5ae93147f5c2a0c712215c9757f
Pratham Gajbhiye <gajbhiyepratham876@gmail.com>
2023-11-08 19:44:59 +0530

    Strings in Java
    Strings and it functions

---
+++ Strings/string_functions.java
@@ -0,0 +1,70 @@
+public class string_functions{
+    public static void main(String[] args) {
+        // 1.length()
+        /* 
+        String s = "Pratham";
+        System.out.println(s.length());
+        */
+
+
+        // 2.charAt()
+        /*
+        String str = "Pratham";
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
+        }
+        */
+
+
+
+        // 3.compareTo()
+        /* 
+        String str = "Pratham";
+        String ptr = "Pratham";
+
+        if((str.compareTo(ptr)) > 0){
+            System.out.println("String 1 is greater");
+        }
+        else if((str.compareTo(ptr)) == 0){
+            System.out.println("Equal");
+        }
+        else{
+            System.out.println("String 2 is greater");
+        }
+        */
+
+
+        // 4.substring()
+        
+        // String s = "Pratham";
+        // System.out.println(s.substring(3));
+        
+
+
+        // 5.contains()
+        /*
+        String str = "Pratham";
+        String p = "pra";
+        System.out.println(str.contains(p.);
+        */
+
+        // 6. concat
+        
+        // String s = "Pratham";
+        // s = s.concat(" is a good boy");
+        // System.out.println(s);
+        
+
+        // 6. replace
+        // String s = "Pratham";
+        // s = s.replace('a', 't');
+        // System.out.println(s);
+
+
+        // 7.indexOf
+        // String str = "Pratham";
+        // System.out.println(str.indexOf("a"));
+        // System.out.println(str.indexOf("a",3));
+    
+    }
+}
---
+++ Strings/stringbuilder_implementation.java
@@ -0,0 +1,5 @@
+public class stringbuilder_implementation {
+    public static void main(String[] args) {
+        
+    }
+}
