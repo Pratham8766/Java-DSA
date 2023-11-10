101b95d6e23648137ffa8bb26e0031022d85bd52
Pratham Gajbhiye <gajbhiyepratham876@gmail.com>
2023-11-10 17:55:45 +0530

    Unary Operatot
    Learnt about the Unary Operator and its working

---
+++ Operators and Binary Numbers/unary.java
@@ -0,0 +1,28 @@
+public class unary{
+    public static void main(String[] args) {
+        //Unary Operator
+        //1. Increment operator
+        int a = 10;
+        int b = a++;
+        System.out.println(a);
+        System.out.println(b);
+        
+        
+        int c = 10;
+        int d = ++c;
+        System.out.println(c);
+        System.out.println(d);
+
+        // 2. Decrement operator
+        int v = 10;
+        int z = v--;
+        System.out.println(v);
+        System.out.println(z);
+        
+        
+        int x = 10;
+        int y = --x;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
