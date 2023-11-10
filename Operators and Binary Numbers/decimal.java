05bd9a6fadae745afb4240364d108429a9ba2605
Pratham Gajbhiye <gajbhiyepratham876@gmail.com>
2023-11-10 17:55:13 +0530

    Decimal
    Learnt that the number system in java is called as Decimal System

---
+++ Operators and Binary Numbers/decimal.java
@@ -0,0 +1,8 @@
+public class decimal {
+    public static void main(String[] args) {
+        //Maths Number in Programming is known as DECIMAL NUMBER (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+        //The program is converted into Binary in the backend
+
+    }
+}
