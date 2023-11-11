930cf9e940afc4273fcb3bf39718f3ce55173adf
Pratham Gajbhiye <gajbhiyepratham876@gmail.com>
2023-11-11 20:51:42 +0530

    Clear and Update Bit Operation
    Learnt about Clear and Update Bit operation in Bit Manipulation

--- Bit Manipulation/bitmanipulation.java
+++ Bit Manipulation/bitmanipulation.java
@@ -16,10 +16,12 @@ public class bitmanipulation {
          */
 
         // 1. Get Operation
-        // Get the 3rd bit (position = 2) of a number n (n = 0101)
+        /*  
+        Get the 3rd bit (position = 2) of a number n (n = 0101)
 
-        // Bit mask = 1<<i
-        // Operation = AND
+        Bit mask = 1<<i
+        Operation = AND
+        */
 
         // int n = 5;
         // int position = 1;
@@ -40,13 +42,81 @@ public class bitmanipulation {
         // Bit mask = 1<<i
         // Operation = OR
 
-        int n = 5;
-        int position = 1;
-        int bitmask = 1<<position;
-        int result = n | bitmask;
+        // int n = 5;
+        // int position = 1;
+        // int bitmask = 1<<position;
+        // int result = n | bitmask;
         
-        System.out.println(result);
+        // System.out.println(result);
 
-        // if the resultant value is non zero then the bit is 1 and if zero then 0
+
+        // 3. Clear Operation
+        /*
+        Set the 2nd bit (position = 1) of a number n (n = 0101)
+
+        Bit mask = 1<<i
+        Operation = AND WITH NOT
+
+         Calculate NOT of Bitmask
+         then calculate NOT with AND of original Number
+         */
+
+        // int number = 5;
+        // int position = 2;
+        // int bitmask = 1<<position; 
+        
+
+        // int not = ~bitmask;
+        // int newNumber = number&not;
+        // System.out.println(newNumber);
+
+
+        // 4. Update Operation
+        /*
+         For 1
+        Bit mask = 1<<position
+        Operation = AND with NOT
+
+        For 2
+        Bit mask = 1<<position
+        Operation = OR
+
+
+        0101
+        1011
+        0001
+         */
+
+        // For changing 1 to 0
+        // int number = 5;
+        // int position = 2;
+        // int bitmask = 1 << position;
+        
+        // int notBitMask = ~(bitmask);
+        // int newNumber = notBitMask & number;
+        // System.out.println(newNumber);
+
+        // For changing 0 to 1
+        // int number = 5;
+        // int position = 1;
+        // int bitmask = 1 << position;
+        // int newNumber = bitmask | number;
+        // System.out.println(newNumber);
+
+        // int number = 5;
+        // int position = 1;
+        // int changingBit = 0;
+
+        // if(changingBit==0){
+        //     int bitMask = 1 << position;
+        //     int newNumber = bitMask | number;
+        //     System.out.println(newNumber); 
+        // }
+        // else{
+        //     int newBitMask = 1 << position;
+        //     int nNewBitMask = ~(newBitMask);
+        //     int newNumber1 = nNewBitMask & number;
+        //     System.out.println(newNumber1);
+        // }
     }
 }
