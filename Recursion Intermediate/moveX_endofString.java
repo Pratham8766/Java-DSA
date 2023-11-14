a35e7cb02ed814aa7799520d5bd9bab840bd51cb
Pratham Gajbhiye <gajbhiyepratham876@gmail.com>
2023-11-14 17:58:15 +0530

    Move Character to the end of the string
    Recursion Intermediate problem of moving x to the end of the string

---
+++ Recursion Intermediate/moveX_endofString.java
@@ -0,0 +1,25 @@
+public class moveX_endofString {
+    public static void moveX(String s, int idx, int count, String n){
+        if(s.length()==idx){
+            for (int i = 0; i < count; i++) {
+                n+='x';
+            }
+            System.out.println(n);
+            return;
+        }
+        char currChar = s.charAt(idx);
+        if(currChar=='x'){
+            count++;
+            moveX(s, idx+1, count, n);
+        }
+        else{
+            n+=currChar;
+            moveX(s, idx+1, count, n);
+        }
+    }
+    public static void main(String[] args) {
+        String s = "abcxxdusax";
+        String p = "";
+        moveX(s, 0, 0, p);
+    }
+}
