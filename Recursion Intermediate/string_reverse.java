be3fbf0b30b0340b378e0def1a71c271dc83723b
Pratham Gajbhiye <gajbhiyepratham876@gmail.com>
2023-11-14 15:00:16 +0530

    Recursion Intermediate
    Learnt About Printing String in Reverse order Character by Character

---
+++ Recursion Intermediate/string_reverse.java
@@ -0,0 +1,13 @@
+public class string_reverse {
+    public static void stringReverse(int i, String s){
+        if(i==0){
+            System.out.println(s.charAt(i));
+            return;
+        }
+        System.out.println(s.charAt(i));
+        stringReverse(i-1, s);
+    }
+    public static void main(String[] args) {
+        stringReverse(3, "abcd");
+    }    
+}
