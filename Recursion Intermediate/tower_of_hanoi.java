efd515585983a0e3757ce46a6cce947b43705886
Pratham Gajbhiye <gajbhiyepratham876@gmail.com>
2023-11-14 13:57:43 +0530

    Tower of Hanoi Problem
    Learnt Tower of Hanoi Problem

---
+++ Recursion Intermediate/tower_of_hanoi.java
@@ -0,0 +1,15 @@
+public class tower_of_hanoi {
+    public static void TOH(int n, String src, String help, String Dest){
+        if(n==1){
+            System.out.println("Transfer Disk "+n+ " from " + src + " to "+ Dest);
+            return;
+        }
+        TOH(n-1, src, Dest, help);
+        System.out.println("Transfer Disk "+n+ " from " + src + " to "+ Dest);
+        TOH(n-1, help, src, Dest);
+    }
+    public static void main(String[] args) {
+        int n = 3;
+        TOH(n, "Source Tower", "Helper Tower", "Destination Tower");
+    }
+}
