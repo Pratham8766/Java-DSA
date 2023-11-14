0b887e7b2a1c838b080936c7a2687aaab30c95e3
Pratham Gajbhiye <gajbhiyepratham876@gmail.com>
2023-11-14 18:52:39 +0530

    Remove Duplicates from String
    Remove Duplicates from the String including Uppercase as well as Lowercase letters

---
+++ Recursion Intermediate/remove_duplicates_from_string.java
@@ -0,0 +1,29 @@
+public class remove_duplicates_from_string {
+    private static int toIndex(char c) {
+        if (Character.isUpperCase(c)) {
+            return c - 'A';
+        } else {
+            return c - 'a' + 26;
+        }
+    }
+
+    public static boolean map [] = new boolean[52];
+    public static void remove_duplicate(String s, int idx, String newString){
+        if(s.length()==idx){
+            System.out.println(newString);
+            return;
+        }
+        
+        if(map[toIndex(s.charAt(idx))] == true){
+            remove_duplicate(s, idx+1, newString);
+        }
+        else{
+            newString+=s.charAt(idx);
+            map[toIndex(s.charAt(idx))] = true;
+            remove_duplicate(s, idx+1, newString);
+        }
+    }
+    public static void main(String[] args) {
+        remove_duplicate("HappyNewYear",0,"");
+    }
+}
