4dcfc3fef209c365627c309626f88c2aff34435e
Pratham Gajbhiye <gajbhiyepratham876@gmail.com>
2023-11-14 17:34:06 +0530

    Check if array is sorted
    Check if array is sorted intermediate recursion problem.

---
+++ Recursion Intermediate/check_sorted_array.java
@@ -0,0 +1,18 @@
+public class check_sorted_array {
+    public static boolean isSorted(int [] arr, int idx){  
+        if(arr.length-1==idx){
+            return true;
+        }     
+        if(arr[idx]<arr[idx+1]){
+            return isSorted(arr, idx+1);
+        }
+        else{
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,3};
+        boolean b = isSorted(arr, 0);
+        System.out.println(b);
+    }
+}
