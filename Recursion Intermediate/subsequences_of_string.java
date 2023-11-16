af0c811e178ac8a1a7935f4038c6c36473fe2a47
Pratham Gajbhiye <gajbhiyepratham876@gmail.com>
2023-11-16 18:53:51 +0530

    Subsequences of a string
    Subsequences of a string Problem learned and solved

---
+++ Recursion Intermediate/subsequences_of_string.java
@@ -0,0 +1,16 @@
+public class subsequences_of_string {
+    public static void printSubsequence(String str, int idx, String newString){
+        if(idx==str.length()){
+            System.out.println(newString);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        
+        printSubsequence(str, idx+1, newString+currChar);
+
+        printSubsequence(str, idx+1, newString);
+    }
+    public static void main(String[] args) {
+        printSubsequence("abcd", 0, "");
+    }
+}
