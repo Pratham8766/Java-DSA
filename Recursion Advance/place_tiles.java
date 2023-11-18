cbf625444b9e6837f47561cb79fbfc3f7d745837
Pratham Gajbhiye <gajbhiyepratham876@gmail.com>
2023-11-18 14:30:37 +0530

    Place tiles Problem

---
+++ Recursion Advance/place_tiles.java
@@ -0,0 +1,19 @@
+//Place tiles of size 1xm in a floor of size nxm
+public class place_tiles{
+    public static int count(int n, int m){
+        if(n<m){
+            return 1;
+        }
+        if(n==m){
+            return 2;
+        }
+        int horizontalPlacement = count(n-1,m);
+        int verticalPlacement = count(n-m,m);
+
+        return horizontalPlacement+verticalPlacement;
+    }
+    public static void main(String[] args) {
+        int n=4, m=2;
+        System.out.println(count(n, m));
+    }
+}
