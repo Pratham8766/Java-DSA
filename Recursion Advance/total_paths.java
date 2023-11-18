1a456f1d3b200316cad6f524af4df2b8279b11de
Pratham Gajbhiye <gajbhiyepratham876@gmail.com>
2023-11-18 14:13:03 +0530

    Total Paths Problem

---
+++ Recursion Advance/total_paths.java
@@ -0,0 +1,17 @@
+public class total_paths {
+    public static int totalNoOfPaths(int i, int j, int n, int m){
+        if(i==n || j==m){
+            return 0;
+        }
+        if(i==n-1 && j==m-1){
+            return 1;
+        }
+        int firstPath = totalNoOfPaths(i+1, j, n, m);
+        int secondPath = totalNoOfPaths(i, j+1, n, m);
+        return firstPath+secondPath;
+    }
+    public static void main(String[] args) {
+        int n = 3, m = 3;
+        System.out.println(totalNoOfPaths(0, 0, n, m));
+    }
+}
