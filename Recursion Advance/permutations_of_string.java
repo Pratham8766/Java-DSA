f699b7b5a3a57c10fe989ac0f794f802443c458e
Pratham Gajbhiye <gajbhiyepratham876@gmail.com>
2023-11-18 13:57:54 +0530

    Pemutations of String Problem

--- Recursion Advance/permutations_of_string.java
+++ Recursion Advance/permutations_of_string.java
@@ -1,16 +1,15 @@
 public class permutations_of_string {
-    public static void printPermutations(String str, String permutations) {
-        if(str.length()==0){
-            System.out.println(permutations);
+    public static void printPermutations(String s, String permutationString){
+        if(s.length()==0){
+            System.out.println(permutationString);
             return;
         }
-        for (int i = 0; i < str.length(); i++) {
-            char currChar = str.charAt(i);
-            String newStr = str.substring(0, i)+str.substring(i+1);
-            printPermutations(newStr, permutations+currChar);
+        for (int i = 0; i < s.length(); i++) {
+            char currChar = s.charAt(i);
+            String newStr = s.substring(0,i)+s.substring(i+1);
+            printPermutations(newStr, permutationString+currChar);
         }
     }
-
     public static void main(String[] args) {
         printPermutations("abc","");
     }
